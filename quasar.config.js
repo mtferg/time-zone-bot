e14c38b9f4625da65898bb36675ec49338c6ca8e
Michael Ferguson <michael.tho.ferguson@gmail.com>
2025-10-06 19:01:38 -0600

    GitHub Pages Config (#5)
    * buy me a coffee
    
    * github action

--- quasar.config.js
+++ quasar.config.js
@@ -67,7 +67,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: process.env.PUBLIC_PATH || '/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
